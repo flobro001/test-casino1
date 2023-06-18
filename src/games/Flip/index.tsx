413c12f8659133bc4500ce10c761bf0d6bb0400e
flobro001 <78507971+flobro001@users.noreply.github.com>
2023-06-18 23:45:26 +0200

    Update index.tsx

--- src/games/Flip/index.tsx
+++ src/games/Flip/index.tsx
@@ -5,7 +5,7 @@ export default {
   name: 'Flip',
   short_name: 'flip',
   description: '',
-  creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',
+  creator: 'CGsMk2RXU2CkskAsGwEgi2fx895D5Y2PMtBdjWvGzVuJ',
   image,
   theme_color: '#ad6bff',
   app: React.lazy(() => import('./App')),
